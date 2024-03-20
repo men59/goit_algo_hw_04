b78210f487430abd553070c0cfd816cd4bf17eef
men59 <mnazarchuk@seznam.cz>
2024-03-20 05:41:47 +0100

    Initial commit

---
+++ goit_algo_hw_04_01.py
@@ -0,0 +1,19 @@
+def total_salary(path):
+    try:
+        with open(path, "r") as fh:
+            salary = []
+            while True:
+                line = fh.readline()
+                if not line:
+                    break
+                _line = line.split(',')
+                salary.append(int(_line[0]))  # Перше число в кожному рядку
+            total = sum(salary)
+            count = len(salary)
+            average = total / count
+            return total, average
+    except FileNotFoundError:
+        print("Файл не знайдено")
+        return None, None
+total, average = total_salary("file.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
